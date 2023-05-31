65f7798b2024faeb370fbd3c7231918699077def
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-05-31 21:00:06 +0700

    Update Auth

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Users, UsersSchema } from '../users/schemas/users.schema';
+import { AppModule } from '../app.module';
+import { JwtModule } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        AppModule,
+        MongooseModule.forFeature([
+          {
+            name: Users.name,
+            schema: UsersSchema,
+          },
+        ]),
+        JwtModule.register({
+          secret: process.env.SECRET,
+          signOptions: { expiresIn: '1h' },
+        }),
+      ],
       providers: [AuthService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  const timestamp = new Date().getTime();
+
+  const register = {
+    username: `test${timestamp}`,
+    email: `test${timestamp}@mail.com`,
+    password: 'testing1234',
+  };
+
+  const login = {
+    username: `test${timestamp}`,
+    email: `test${timestamp}@mail.com`,
+    password: 'testing1234',
+  };
+
+  it('register', async () => {
+    expect(await service.register(register)).toEqual({
+      message: 'User has been created successfully',
+    });
+  });
+
+  it('login', async () => {
+    expect(await service.login(login)).toHaveProperty('access_token');
   });
 });
